b870b79cc7339d827da25d94123504c3e0bafa13
Ian Douglas <ijd@idouglas.org>
2016-08-23 12:48:42 -0400

    Add files via upload
    Updates for this morning.  Got a 'good' csv output -- but the KF with IMU data is "linearized" -- a real goofy output.  Corrected chart looks better.  Graph axis still doesn't have 42.0 it shows 4.20 E+01 and the titles don't show up.  I'd like to add a 0.001 pad to the x-axis and y-axis.  Not sure how to format the x- and y-axis or force the titles in the plot_first_dim routine.

--- pykal5.py
+++ pykal5.py
@@ -3,7 +3,7 @@
 '''
 # <codecell>    pykal5.py    Kalman Filter for GPS trail (path trace) smoothing
 #
-#    Ian Douglas    2015-August
+#    Ian Douglas   forked 2015-August updated through 2016-August
 '''
 
 from pykalman import KalmanFilter
@@ -47,7 +47,7 @@ def gps_to_cartesian(coordinates,inverted=False):
 
 def calculate_point_variance(measurements, corrected,absolute=True):
     if len(measurements) != len(corrected):
-            print "calculate_point_variance: Lists must be same size", len(measurements), len(corrected)
+            print "* 50 * calculate_point_variance: Lists must be same size", len(measurements), len(corrected)
             exit(1)
         
     variance = []    
@@ -93,7 +93,7 @@ def positions_from_offsets_additive(ref,offsets):
 def positions_from_offsets_corrective(refs,offsets):
     
     if not (len(refs) == len(offsets)):
-        print "ERROR: positions from offsets not matching in length. %d  vs.  %d" % (len(refs), len(offsets))
+        print "ERROR 96: positions from offsets not matching in length. %d  vs.  %d" % (len(refs), len(offsets))
         exit(1)
     
     positions = []
@@ -124,7 +124,7 @@ def run_kal(measurements, training_size=150):
     else:
         print "Error: Dimensions for run_kal must be 3 or 9"
         exit(1)
-    print("run_kal smooth --> simple_mode={0}".format(simple_mode))
+    print("run_kal 127 - run_kal smooth --> simple_mode={0}".format(simple_mode))
     
     training_set = []
     for i in range(min(training_size,len(measurements))):
@@ -149,8 +149,8 @@ def load_gps_trail(extra_fields = False):
     i = 0
     measurements = []
     header=True
-    print("Failure here means the input gps file did not have correct Unix LF endings...")
-    for line in open('nazeer_trip_400-2100-LF.csv'):        #'nazeer_trip.csv'):
+    print("load_gps_trail 152 - Failure here means the input gps file did not have correct Unix LF endings...")
+    for line in open('nazeer-2735-3485-xyz-vel-accel.csv'):        #'nazeer_trip.csv'):
         if header:
             header=False
             continue
@@ -161,10 +161,10 @@ def load_gps_trail(extra_fields = False):
             print i,fields
             continue
         elif i == 1:
-            print i,fields, ', Length of line = ', len(fields)
+            print i,fields, '\n Length of line = ', len(fields), " extra_fields = ", extra_fields
             continue
         elif i == 100:
-            print i,fields, extra_fields #, measurements
+            print i,fields, "\n extra_fields = ", extra_fields #, measurements
             continue
 
 #       print "Past the 6 - 1 and 100 clause", i,fields
@@ -204,6 +204,8 @@ def plot_first_dim(data,flip_xy=True,xlbl="",ylbl="",title="",filename=None):
     else:
         minx = maxx = data[0][0]
         miny = maxy = data[0][1]
+    
+    print "plot_first_dim 208 - length data = ", len(data)
     
     for x,y,z, xdot, ydot, zdot, xddot, yddot, zddot in data:
         
@@ -254,7 +256,6 @@ def plot_all_dimensions(positions,file_prefix):
     plot_first_dim(z_vals,flip_xy=False,xlbl="Time (s)",ylbl="Altitude ",title="Altitude OVER TIME",filename=output_dir +file_prefix + "all_dim_z")
 #   END plot_all_dimensions    
 
-
 '''    
 ## MAIN Program here
 '''
@@ -270,22 +271,27 @@ parse_extra_fields = True
 
 # Load the path in from the file 
 measurements = load_gps_trail(extra_fields = parse_extra_fields )
-print "Main Line 271 - measurements ", measurements, parse_extra_fields
+print "Main 274 - measurements ", measurements, "\n273 parse_extra_fields = ", parse_extra_fields
 
 # Convert these measurements to meters
 cart_measurements = gps_to_cartesian(measurements)
-print "Main Line 275 - cart_measurements ", cart_measurements
+print "Main 278 - cart_measurements ", cart_measurements
 
 #############################
 ##  Positions              ##
 #############################
 
 # Run the Kalman filter on the measurements
-print "Run KF - measurements", measurements
+print "Main 285 - Run KF - measurements", measurements
 ss_means_meas, ss_covariance_meas,cart_simple = run_kal(measurements,training_size=150)
 filtered_measurements = ss_means_meas        # Give this a more meaningful name
-print "Run KF - filtered_measurements", filtered_measurements
-
+print "Main 288 - Run KF - filtered_measurements " , len(filtered_measurements)
+f = open(output_dir + 'skf_trail_filtered.csv', 'w')
+f.write('lat,lng,alt,xdot,ydot,zdot,xddot,yddot,zddot\n')
+for lat,lng,alt,xdot,ydot,zdot,xddot,yddot,zddot in filtered_measurements:
+   f.write("{0},{1},{2},{3},{4},{5},{6},{7},{8}\n".format(lat,lng,alt,xdot,ydot,zdot,xddot,yddot,zddot))
+f.close()
+print "Main 294"
 #############################
 ##  OFFSETS                ##
 #############################
@@ -297,7 +303,6 @@ offsets = calculate_offsets(measurements)
 ss_means, ss_covariance,offset_simple = run_kal(offsets)
 corrected_offsets = ss_means    # Give this a more meaningful name
 
-
 # Reconstruct the cartesian positions from the offsets and a reference point
 #corrected_cart_positions = positions_from_offsets_additive(cart_measurements[0],corrected_offsets)
 corrected_positions = positions_from_offsets_corrective(measurements[1:len(measurements)], corrected_offsets)
@@ -306,20 +311,24 @@ corrected_positions = positions_from_offsets_corrective(measurements[1:len(measu
 corrected_gps_positions = gps_to_cartesian(corrected_positions,inverted=True)
 
 ### OFFSET Results ##
+print "Main 314 - plot_first_dim - measurements[:3] "
 plot_first_dim(
     measurements[:3],
     title = "Measurements in WGS84 - Lat/Long/alt",
     xlbl = 'X Position (latitude degrees)',
     ylbl = 'Y Position (longitude degrees)',
     filename = output_dir + "measurements.png" )
-    
+
+print "Main 322 - plot_first_dim - corrected_positions "
 plot_first_dim(
     corrected_positions,
     title = "Corrected GPS Points from Offsets ", 
     xlbl = 'X Position (latitude degrees)',
     ylbl = 'Y Position (longitude degrees)',
-    filename = output_dir + "filtered_fromoff.png")
-    
+    filename = output_dir + "corrected_fromoff.png")
+#    filename = output_dir + "filtered_fromoff.png")
+
+print "Main 331 - plot_first_dim - filtered_measurements "
 plot_first_dim(
     filtered_measurements,
     title = "Corrected WGS84 Points from Positions", 
@@ -327,9 +336,9 @@ plot_first_dim(
     ylbl = 'Y Position (longitude degrees)',
     filename = output_dir + "filtered_frompos.png")
 
-plot_all_dimensions(corrected_positions,"fromFilteredOffset-")
-plot_all_dimensions(measurements,"fromMeasurements-")
-
+print "Main 339 - length corrected_positions = ", len(corrected_positions), " length measurements = ", len(measurements)
+plot_first_dim(corrected_positions,"fromFilteredOffset-")
+plot_first_dim(measurements,"fromMeasurements-")
 
 # Split the variance into it's three components and draw it
 
@@ -341,7 +350,7 @@ for x,y,z in offset_variance:
     offset_variance_split[0].append(x)
     offset_variance_split[1].append(y)
     offset_variance_split[2].append(z)
-    
+
 plt.plot(offset_variance_split[0])
 plt.plot(offset_variance_split[1])
 plt.plot(offset_variance_split[2])
@@ -350,7 +359,6 @@ plt.title('Variance Between Measurement and Filtered (Offsets)')
 plt.ylabel('Measurement offset_variance')
 #plt.show()
 plt.savefig(output_dir + "offset_variance.png")
-
 
 # calculate variance for positions
 position_variance = calculate_point_variance(measurements, filtered_measurements)
@@ -369,21 +377,28 @@ plt.ylabel('Measurement offset_variance')
 #plt.show()
 plt.savefig(output_dir + "point_variance.png")
 
-
-
 # output corrected file
 
 f = open(output_dir + 'gps_trail_filtered.csv', 'w')
 f.write('lat,lng,alt\n')
+i = 0
 for x,y,z in corrected_gps_positions:
-    f.write("{0}, {1}, {2}\n".format(x,y,z))
-    
+   i += 1
+   f.write("{0}, {1}, {2}\n".format(x,y,z))
+   if i % 6 == 0:     # Display every sixth entry, plus first and 100th
+      print i,x,y,z
+      continue
+   elif i == 1:
+       print i,x,y,z, '\n Length of line = ', len(corrected_gps_positions)
+       continue
+   elif i == 100:
+       print i,x,y,z #, "\n extra_fields = ", extra_fields #, measurements
+       continue
+
 f.close()
-    
-    
 
 '''
-##    GPS Trail input and Kalman filter smoothing...
+##    GPS Trail input and standard Kalman filter smoothing...
 ##
 ##  End <pykal5.py>
 '''
